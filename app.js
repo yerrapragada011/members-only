4487e915603181ca38f70b9948439bc242dc547d
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-08-23 14:14:43 -0400

    Fix port issue

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.use(passport.session())
 
 app.use('/', indexRouter)
 
-const port = 3000
-app.listen(port, () => {
+const port = process.env.PGPORT || 3000
+
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
 })
